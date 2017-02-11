4c201f8c8da7e9c578f0da1bf6d77137ff4b4460
Alex <alexrafael10@gmail.com>
2017-02-11 00:14:25 -0400

    AreaActividad Model

---
+++ app/AreaActividad.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AreaActividad extends Model
+{
+  protected $table = "area_actividad";
+
+  protected $fillable = [
+            'id',
+            'id_area',
+            'id_actividad'
+          ];
+
+  public function area(){
+    return $this->belongsTo('App\Area','id_area');
+  }
+
+  public function evento(){
+    return $this->belongsTo('App\Evento','id_actividad');
+}
