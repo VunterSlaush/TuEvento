f4cacde6e7e71d04d8b1f5a54eab23c0ba250d53
Alex <alexrafael10@gmail.com>
2017-02-11 00:08:38 -0400

    Area Model

---
+++ app/Area.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Area extends Model
+{
+  protected $table = "area";
+
+  protected $fillable = ['id','nombre'];
+
+  public function areaEvento(){
+    return $this->hasMany('App\AreaEvento','id_area');
+  }
+
+  public function areaActividad(){
+    return $this->hasMany('App\AreaActividad','id_area');
+  }
+
+  public function areaJurado(){
+    return $this->hasMany('App\AreaJurado','id_area');
+  }
+}
