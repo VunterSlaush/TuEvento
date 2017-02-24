294344d5a58069c42cf2d37560e028cc3bb8587a
Alex <alexrafael10@gmail.com>
2017-02-23 23:37:29 -0400

    Scheduler view

--- resources/views/evento/organizar.blade.php
+++ resources/views/evento/organizar.blade.php
@@ -5,28 +5,42 @@
     <h1> Organizador</h1>
 
   {{Html::ul($errors->all())}}
-  {{Form::model($evento, array('route' => array('evento.update', $evento->id), 'method' => 'PUT'))}}
 
-    <div class="row">
-      <div class="col m6">
-        {{Form::label('hora_inicio','Inicio')}}
-        {{Form::time('hora_inicio')}}
-      </div>
-      <div class="col m6">
-        {{Form::label('hora_fin','Fin')}}
-        {{Form::date('hora_fin')}}
-      </div>
-      <div class="col m6">
-        {{Form::label('fecha','Fecha')}}
-        {{Form::date('fecha')}}
-      </div>
-      <div class="col m6">
-        {{Form::button('listo')}}
-      </div>
-      {{Form::submit('Organizar')}}
-
-      {{Form::close()}}
-    </div>
-
+    <table>
+      <thead>
+        <tr>
+          <th> Titulo </th>
+          <th> Inicio </th>
+          <th> Fin </th>
+          <th> Fecha </th>
+          <th> Accion</th>
+        </tr>
+      </thead>
+      <tbody>
+        {{Form::model($evento, array('route' => array('evento.update', $evento->id), 'method' => 'PUT'))}}
+          @foreach($actividades as $actividad)
+            <tr>
+              <td>
+                {{$actividad->titulo}}
+              </td>
+              <td>
+                {{Form::time('hora_inicio',$actividad->hora_inicio)}}
+              </td>
+              <td>
+                {{Form::time('hora_fin',$actividad->hora_fin)}}
+              </td>
+              <td>
+              {{Form::date('fecha',$actividad->fecha)}}
+              </td>
+              <td>
+                <input type="checkbox" id="listo" name="listo" />
+                <label for="listo">listo</label>
+              </td>
+            </tr>
+          @endforeach
+       {{Form::submit('Organizar')}}
+       {{Form::close()}}
+      </tbody>
+    </table>
   </div>
 @endsection
