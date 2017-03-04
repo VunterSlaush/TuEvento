0c2f79fd772228dda4343b33bd8ca66e910c81de
Israel Maita <imaita@e.uneg.edu.ve>
2017-03-03 22:46:22 -0400

    PDF Style Finished

--- resources/views/certificado.blade.php
+++ resources/views/certificado.blade.php
@@ -4,11 +4,13 @@
     <title>Certificado</title>
     <link href="css/certificado.css" rel="stylesheet">
 </head>
-<body background-image="img/back-certs.jpg">
-	<br>
+<body>
+	<br />
 
 	<center>
-
+	
+	<img src="http://vignette4.wikia.nocookie.net/batman/images/a/a7/Batman_Logo_04.png/revision/latest/scale-to-width-down/640?cb=20160614170334" style="height:150px; width:150px;">
+	
 	@if($certificate->imagen != '')
 
 	<img src="http://vignette4.wikia.nocookie.net/batman/images/a/a7/Batman_Logo_04.png/revision/latest/scale-to-width-down/640?cb=20160614170334" style="height:150px; width:150px;">
@@ -17,16 +19,18 @@
 
 		<h3>Certificado que se otorga a:</h3>
 
-		<br>
+		<br />
 
 		<h1>{{ $certificate->nombre }}</h1>
 
+		<br />
+
 		@if(property_exists($certificate,'titulo'))
-			<h3>Por su valiosa asistencia y participación en <b><i>{{ $certificate->titulo }}</i></b>,<br>
-			durante el evento <b><i>{{ $certificate->evento }}</i></b>,<br>llevado a cabo en {{ $certificate->lugar }} el {{$certificate->fecha }}.</h3>
+			<h3>Por su valiosa asistencia y participación en <br /> <i>{{ $certificate->titulo }}</i>,</h3><h4>durante el evento <b><i>{{ $certificate->evento }},</i></b></h4><h4>llevado a cabo en {{ $certificate->lugar }} el {{$certificate->fecha }}.</h4>
 		@else
-			<h3>Por su valiosa asistencia y participación en el evento <b><i>{{ $certificate->evento }}</i></b>,<br> llevado a cabo en {{ $certificate->lugar }} el {{$certificate->fecha }}.</h3>
-			@endif
+			<h3>Por su valiosa asistencia y participación en el evento <br /> <i>{{ $certificate->evento }}</i>,</h3>
+			<h4>llevado a cabo en {{ $certificate->lugar }}, el {{$certificate->fecha }}.</h4>
+		@endif
 
 		<br>
 
@@ -37,9 +41,9 @@
 		<br>
 
 		@if(property_exists($certificate,'codigo'))
-			<p>Código de verificación del certficado: {{$certificate->codigo}}. </p>
+			<p>Código de verificación: {{$certificate->codigo}}. </p>
 		@else
-			<p>Código de verificación del certficado: {{$certificate->cedula}}-{{$certificate->evento_id}}.</p>
+			<p>Código de verificación: {{$certificate->cedula}}-{{$certificate->evento_id}}.</p>
 		@endif
 
 	</center>
