470b60765989ecc57807e90f025f8e74328d9e01
Alex <alexrafael10@gmail.com>
2017-03-04 13:33:51 -0400

    make "fecha" on "activity" nullable

--- database/migrations/2017_01_14_024215_create_actividad_table.php
+++ database/migrations/2017_01_14_024215_create_actividad_table.php
@@ -13,7 +13,7 @@ class CreateActividadTable extends Migration
     {
         Schema::create('actividad', function (Blueprint $table) {
             $table->increments('id');
-            $table->date('fecha');
+            $table->date('fecha')->nullable();
             $table->string('titulo');
             $table->time('hora_inicio')->nullable();
             $table->time('hora_fin')->nullable();
